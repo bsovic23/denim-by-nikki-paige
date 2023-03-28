6d97c74695bdc62d9f3f15420ceeee5eb8eeb517
Brit Sovic <bsovic@tulane.edu>
2023-03-28 10:26:34 -0400

    created mongoose modeling

---
+++ server/models/index.js
@@ -0,0 +1,11 @@
+const Category = require('./Category');
+const Order = require('./Order');
+const Product = require('./Product');
+const User = require('./User');
+
+module.exports = {
+    Category,
+    Order,
+    Product,
+    User
+};
